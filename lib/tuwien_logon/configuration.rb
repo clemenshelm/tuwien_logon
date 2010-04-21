daa40019d96a6961adac075085b60ce399c5ecc7
Clemens Helm <clemens.helm@gmail.com>
2010-04-21 20:22:21 +0200

    removed authentication_url config option

--- lib/tuwien_logon/configuration.rb
+++ lib/tuwien_logon/configuration.rb
@@ -4,10 +4,9 @@ module TuwienLogon
   class Configuration
     include Singleton
     
-    attr_accessor :authentication_url, :user_info_url, :secret, :user_info_params, :time_tolerance, :server_time_offset
+    attr_accessor :user_info_url, :secret, :user_info_params, :time_tolerance, :server_time_offset
     
     def initialize
-      @authentication_url = 'https://iu.zid.tuwien.ac.at/AuthServ.authenticate'
       @user_info_url = 'https://iu.zid.tuwien.ac.at/AuthServ.userInfo'
       @secret = '123456'
       @user_info_params = [:oid, :firstname, :lastname, :title, :matriculation_number, :institute_symbol]
--- test/helper.rb
+++ test/helper.rb
@@ -8,7 +8,6 @@ require 'tuwien_logon'
 
 class Test::Unit::TestCase
   def setup
-    TuwienLogon.config.authentication_url = 'http://localhost:4567/AuthServ.authenticate'
     TuwienLogon.config.user_info_url = 'http://localhost:4567/AuthServ.userInfo'
     TuwienLogon.config.secret = '123456'
     TuwienLogon.config.user_info_params = [:oid, :firstname, :lastname, :title, :matriculation_number, :institute_symbol]
